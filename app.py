9910a727f7db958409d2252901e5d3521b4120bf
Nikita Ivanov <mybestjob@myself.com>
2021-07-11 19:47:34 +0300

    Adding migrations

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from logging import error
 from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Lenovo:Privet007@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description=db.Column(db.String(), nullable=False)
+    completed=db.Column(db.Boolean, nullable=True, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
-
-db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
