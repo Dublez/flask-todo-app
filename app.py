21196374a5e3a65c188b316fb9e0080a459f1ae2
Nikita Ivanov <mybestjob@myself.com>
2021-07-23 02:12:00 +0300

    Bulk update list

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ class TodoList(db.Model):
     __tablename__ = 'todolists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable = False)
+    completed = db.Column(db.Boolean, nullable = True, default = False)
     todos = db.relationship('Todo', backref='list', lazy = True)
 
     def __repr__(self):
@@ -74,6 +75,23 @@ def create_todo():
     else:
         return jsonify(body)
 
+@app.route('/lists/<list_id>/set-completed', methods=['POST'])
+def set_completed_list(list_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        list = TodoList.query.get(list_id)
+        list.completed = completed
+        todos = list.todos
+        for todo in todos:
+            todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
     try:
@@ -103,7 +121,7 @@ def delete_todo(todo_id):
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template('index.html', 
-    lists=TodoList.query.all(),
+    lists=TodoList.query.order_by('id').all(),
     active_list=TodoList.query.get(list_id),
     todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
